ac26a0b18cce489efbb97438173a7f46d7f57f42
Javipopotter <javivals@ucm.es>
2025-02-10 16:50:41 +0100

    Añadido ScenesManager preparado para ser usado por Game y usar GameScene
    ScenesManager es una máquina de estados que se encarga de administar las esceneas de juego, tiene varios métodos para alternar fácilmente entre escenas guardando el estado exacto de la última.

---
+++ Carom/src/ECS/ScenesManager.h
@@ -0,0 +1,67 @@
+/**
+ * Máquina de estados con punteros inteligentes.
+ *
+ * @file GameSceneMachine.h
+ * @author Rubén Rubio
+ * @date TPV1 2024
+ */
+
+#ifndef GAME_STATE_MACHINE_H
+#define GAME_STATE_MACHINE_H
+
+#include <memory>
+#include <stack>
+
+#include "GameScene.h"
+#include <SDL.h>
+
+/**
+ * Máquina de estados del juego.
+ *
+ * Se comporta como una pila y proporciona métodos update, render y
+ * handleEvent para aplicarlos sobre el estado actual.
+ */
+class ScenesManager
+{
+	// Pila (como las de EDA) de punteros inteligentes a estados del juego
+	std::stack<std::shared_ptr<GameScene>> GameScenes;
+
+public:
+	~ScenesManager();
+
+	// Apila un nuevo estado, que se convierte en el estado actual,
+	// manteniendo el anterior en la pila
+	void pushState(std::shared_ptr<GameScene> state);
+	// Reemplaza el estado actual por el nuevo estado
+	void replaceState(std::shared_ptr<GameScene> state);
+	// Desapila el estado actual y pasa al siguiente en la pila
+	// (no hace nada si no la pila es vacía)
+	void popState();
+
+	// Sobrecargas que reciben un puntero normal
+	void pushState(GameScene* state);
+	void replaceState(GameScene* state);
+
+	bool empty() const;
+	operator bool() const;
+
+	// Métodos que delegan en los métodos homónimos de GameScene
+	// sobre el estado actual (no hacen nada si la pila es vacía)
+	virtual void update();
+	virtual void render() const;
+	virtual void handleEvent();
+};
+
+inline void
+ScenesManager::pushState(GameScene* state)
+{
+	pushState(std::shared_ptr<GameScene>(state));
+}
+
+inline void
+ScenesManager::replaceState(GameScene* state)
+{
+	replaceState(std::shared_ptr<GameScene>(state));
+}
+
+#endif // GAME_STATE_MACHINE_H
