ed4be1156054425cd6b5278d72b37903004e1d2a
JordiLlinaresSanchez <149331069+JordiLlinaresSanchez@users.noreply.github.com>
2025-02-10 16:35:37 +0100

    clase game scene

---
+++ Carom/src/ECS/GameScene.h
@@ -0,0 +1,49 @@
+#ifndef GAME_Scene_H
+#define GAME_Scene_H
+
+#include <list>
+
+#include "GameList.h"
+
+// Declaraciones anticipadas
+class Entity;
+class Game;
+
+/**
+ * Estado abstracto del juego.
+ *
+ * Mantiene una lista de objetos del juego de los que se hace
+ * responsable (se encarga de eliminarlos). También mantiene una
+ * lista de manejadores de eventos, pero no los elimina.
+ */
+class GameScene
+{
+	GameList<Entity> entities;
+
+protected:
+	Game* game;
+
+	GameScene(Game* game);
+
+	void addObject(Entity* obj);
+
+public:
+	virtual ~GameScene();
+
+	virtual void render() const;
+	virtual void update();
+	virtual void handleEvent();
+
+	/// Obtiene el juego al que pertenece el estado
+	Game* getGame() const;
+	/// Elimina los objetos
+	void clear();
+};
+
+inline Game*
+GameScene::getGame() const
+{
+	return game;
+}
+
+#endif // GAME_Scene_H
