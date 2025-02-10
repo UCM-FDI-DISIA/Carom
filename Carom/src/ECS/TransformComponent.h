d2849290d82699974d4018d3cf9b93bbcbdbf9fd
Mika <mika@ucm.es>
2025-02-10 16:35:34 +0100

    Puntero a box2d

--- Carom/src/ECS/TransformComponent.h
+++ Carom/src/ECS/TransformComponent.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "InfoComponent.h"
 #include "Vector2D.h"
+#include <box2D/box2D.h>
 
 namespace ecs{
     class TransformComponent : public InfoComponent{
+        const b2Transform* _b2Transform = nullptr;
         Vector2D _position;
         struct Scale{
             double x;
