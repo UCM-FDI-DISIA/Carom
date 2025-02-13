ba299ff4dc54c9582d95c3505a5de6c06066ae03
Marco <marcgo19@ucm.es>
2025-02-13 13:36:35 +0100

    Añadido gestor base y funcion hash b2Bodyid
    Metodos preliminares.
    Funcion hash para usar b2Bodyid en mapas
    Añadido a CMakeLists src/Physics como carpeta de inclusión de cabeceras

--- Carom/src/Physics/B2Manager.cpp
+++ Carom/src/Physics/B2Manager.cpp
@@ -1,2 +1,10 @@
 #include "B2Manager.h"
+#include <functional>
 
+
+
+B2Manager::B2Manager() {
+    b2WorldDef worldDef = b2DefaultWorldDef();
+    worldDef.gravity = {0.0f, 0.0f};
+    _worldId = b2CreateWorld(&worldDef);
+}
