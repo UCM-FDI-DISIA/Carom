242c41d2ee294dbc75c130b3ec8ff8eb1077f384
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-02-13 13:55:33 +0100

    clase mainScene

--- Carom/src/Scenes/MainScene.cpp
+++ Carom/src/Scenes/MainScene.cpp
@@ -1,1 +1,26 @@
 #include "MainScene.h"
+
+MainScene::MainScene(State* s, Game* g): GameScene(g){
+    setNewState(s);
+}
+
+void MainScene::setNewState(State* s){
+    _currentState->onStateExit();
+    delete _currentState;
+    _currentState = s;
+}
+
+State* MainScene::getCurrentState(){ return _currentState;}
+
+MainScene::~MainScene(){
+    if(_currentState != nullptr) delete _currentState;
+}
+
+void MainScene::update(){
+    State* a_sceneToChange = nullptr;
+    if(_currentState->checkCondition(a_sceneToChange)){
+        setNewState(a_sceneToChange);
+    }
+    _currentState->update();
+    GameScene::update();
+}
