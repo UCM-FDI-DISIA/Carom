2c0acd7bd1261fa65d010ef918f277680b6ebe92
Hugodbr <hugodbr@gmail.com>
2025-02-13 22:40:05 +0100

    renderTexture::render cambios

--- Carom/src/ECS/RenderTextureComponent.cpp
+++ Carom/src/ECS/RenderTextureComponent.cpp
@@ -11,17 +11,12 @@ namespace ecs {
     }
 
     void RenderTextureComponent::init(Entity* entity){
-        // std::cout << "aqui1" << entity->isAlive();
         _transform = entity->getComponent<TransformComponent>();
     }
 
     void RenderTextureComponent::render(Entity* entity) {
-        // _texture->render(_transform->getPosition()->getX(), _transform->getPosition()->getY());
-        // SDL_Rect dest = build_sdlrect(_transform->getPosition(), _transform->getWidth(), _transform->getHeight());
-        // _texture->render(dest, _transform->getRotation());
-        Vector2D vec2d = {20, 20}; 
-        SDL_Rect dest = build_sdlrect(vec2d, 50, 50);
-        _texture->render(dest, 0);
-        std::cout << "aqui: " << _texture->height() << std::endl;
+        // _texture->render(_transform->getPosition().getX(), _transform->getPosition().getY());
+        SDL_Rect dest = build_sdlrect(_transform->getPosition(), _transform->getWidth(), _transform->getHeight());
+        _texture->render(dest, _transform->getRotation());
     }
 }
