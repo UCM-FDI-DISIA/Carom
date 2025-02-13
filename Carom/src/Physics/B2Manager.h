6c44ef4661e69542d4c04652cf1eb867de1532bd
Marco <marcgo19@ucm.es>
2025-02-13 13:36:35 +0100

    Añadido gestor base y funcion hash b2Bodyid
    Metodos preliminares.
    Funcion hash para usar b2Bodyid en mapas
    Añadido a CMakeLists src/Physics como carpeta de inclusión de cabeceras

--- Carom/src/Physics/B2Manager.h
+++ Carom/src/Physics/B2Manager.h
@@ -1,5 +1,45 @@
 #include <box2D/box2D.h>
+#include "Singleton.h"
+#include <unordered_map>
+#include "Entity.h"
 
-class B2Manager {
+namespace std {
+    template <>
+    struct hash<b2BodyId> {
+        size_t operator()(const b2BodyId& bodyId) const {
+            size_t hash_value = 0;
 
+            // Hash de index1 (int32_t)
+            hash_value ^= std::hash<int32_t>()(bodyId.index1) + 0x9e3779b9 + (hash_value << 6) + (hash_value >> 2);
+
+            // Hash de world0 (uint16_t)
+            hash_value ^= std::hash<uint16_t>()(bodyId.world0) + 0x9e3779b9 + (hash_value << 6) + (hash_value >> 2);
+
+            // Hash de generation (uint16_t)
+            hash_value ^= std::hash<uint16_t>()(bodyId.generation) + 0x9e3779b9 + (hash_value << 6) + (hash_value >> 2);
+
+            return hash_value;
+        }
+    };
+}
+
+class B2Manager : public Singleton<B2Manager> {
+private:
+    friend Singleton<B2Manager>;
+    B2Manager();
+    virtual ~B2Manager();
+
+    bool init();
+
+    // en caso de necesitar acceder a la entidad de un componente con el que se ha colisionado
+    // se puede usar este mapa para acceder
+    std::unordered_map<b2BodyId, ecs::Entity*> _bodyEntityMap;
+
+    b2WorldId _worldId;
+
+public:
+    void addBody();
+    void removeBody();
+
+    ecs::Entity* getEntity();
 };
