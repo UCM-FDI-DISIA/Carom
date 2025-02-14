77e4e765e5b6a13ad945494b73b0a77ba1beca00
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-02-14 11:53:47 +0100

    estatico

--- Carom/src/Physics/PhysicsUtils.h
+++ Carom/src/Physics/PhysicsUtils.h
@@ -2,13 +2,13 @@
 
 static class PhysicsConverter{
 public:
-    const int PIXELS_PER_METER = 75;
+    static const int PIXELS_PER_METER = 75;
 
-    int meter2pixel(float meters) {
+    static int meter2pixel(float meters) {
         return meters * PIXELS_PER_METER;
     }
 
-    float pixel2meter(int pixel){
+    static float pixel2meter(int pixel){
         return pixel/PIXELS_PER_METER;
     }
 };
