50d59de327828141c0a09db4f374f1daab578432
andrea-aparicio-lopez <apariciolopez.andrea@gmail.com>
2025-02-17 17:00:15 +0100

    estado scoring

---
+++ Carom/src/StateMachine/ScoringState.cpp
@@ -0,0 +1,9 @@
+#include "ScoringState.h"
+
+#include "InputHandler.h"
+
+void
+ScoringState::onStateEnter() {
+    auto& ihdlr = ih();
+    // ihdlr.
+}
---
+++ Carom/src/StateMachine/ScoringState.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "State.h"
+
+class ScoringState: public State {
+public:
+
+    virtual void update() override;
+
+    bool checkCondition(State*& state) override;
+
+
+    void onStateEnter() override;
+    void onStateExit() override;
+};
