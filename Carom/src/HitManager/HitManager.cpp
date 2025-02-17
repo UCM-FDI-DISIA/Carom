4a254b972e096a5783ee091a41be726c7c72e19a
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-02-17 17:04:33 +0100

    actualizacion de hit manager

--- Carom/src/HitManager/HitManager.cpp
+++ Carom/src/HitManager/HitManager.cpp
@@ -1,13 +1,25 @@
 #include "HitManager.h"
+#include "GameScene.h"
 
 namespace ecs{
-    HitManager::HitManager() : Singleton(){};
+    HitManager::HitManager(GameScene* mainScene) : Singleton(){
+        _mainScene = mainScene;
+    };
 
     void HitManager::clearAllPositions(){
         _positionsRegistered.clear();
     }
 
     bool HitManager::checkForHitPosition(Vector2D hitPos){
+        if(_positionsRegistered.contains(hitPos)) return false;
 
+        //si no se ha registrado el hit en esta posicion hacer cosas que se necesiten
+        //-instanciar UI de puntuaje
+        //añadir la puntuación al puntuaje final
+        //reproducir un sonido
+        //...
+
+        _positionsRegistered.insert(hitPos);
+        return true;
     }
 }
