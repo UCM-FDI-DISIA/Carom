18e4467614dc5005bbc5d9f4157d7a4c5208dc67
Mika <mika@ucm.es>
2025-02-18 18:17:23 +0100

    fixed conflict with TransformCOmponent

--- Carom/src/ECS/TransformComponent.h
+++ Carom/src/ECS/TransformComponent.h
@@ -19,9 +19,7 @@ namespace ecs{
         double _rotation; //In radians
 
     public:
-        __CMPID_DECL__(cmp::TRANSFORM);
-
-        TransformComponent(Entity* ent) : InfoComponent(ent), _position(Vector2D(0, 0)), _scale({1,1}), _rotation(0) {}
+        TransformComponent(Entity* ent);
         virtual ~TransformComponent(){}
 
         // Getters
