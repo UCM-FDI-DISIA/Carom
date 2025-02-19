f2f529f8d1ff8969b0ea872372bb46ae42697784
Hugodbr <hugodbr@gmail.com>
2025-02-19 08:15:24 +0100

    [entity] clean and friends

--- Carom/src/ECS/Entity.cpp
+++ Carom/src/ECS/Entity.cpp
@@ -12,7 +12,6 @@ namespace ecs {
     }
 
     Entity::~Entity(){
-
         for(Component* component : _currentComponents) 
             delete component;
     }
@@ -21,38 +20,18 @@ namespace ecs {
         this->_anchor = std::move(anchor);
     }
 
-    // bool Entity::addComponent(Component* component, ComponentID ID){
-    //     if(_components[ID] != nullptr) return false;
-
-    //     _components[ID] = component;
-    //     _currentComponents.push_back(component);
-
-    //     _components[ID]->init();
-
-    //     return true;
-    // }
-
-    // bool Entity::removeComponent(ComponentID ID){
-
-    //     if(_components[ID] == nullptr) return false;
-
-    //     auto it = find(_currentComponents.begin(), _currentComponents.end(), _components[ID]);
-    //     _currentComponents.erase(it);
-    //     _components[ID] = nullptr;
-
-    //     return true;
-    // }
-
     void Entity::update(){
         for(Component* component : _currentComponents) 
             component->update();
     }
 
     void Entity::render(){
-        for(Component* component : _currentComponents) component->render();
+        for(Component* component : _currentComponents) 
+            component->render();
     }
 
     void Entity::handleEvents(){
-        for(Component* component : _currentComponents) component->handleEvent();
+        for(Component* component : _currentComponents) 
+            component->handleEvent();
     }
 }
