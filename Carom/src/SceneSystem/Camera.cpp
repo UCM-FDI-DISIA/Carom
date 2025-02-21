c10c358e2ceba81e2efa6bb5c9ba67bb26111ee8
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-02-21 11:34:57 +0100

    arreglado fallo

--- Carom/src/SceneSystem/Camera.cpp
+++ Carom/src/SceneSystem/Camera.cpp
@@ -6,7 +6,7 @@ std::pair<int,int> Camera::getRenderPos(b2Vec2 physicalPos){
     std::pair<int,int> a_camScreenPos = PhysicsConverter::meter2pixel({x,y});
     std::pair<int,int> a_objectPos = PhysicsConverter::meter2pixel(physicalPos);
 
-    return {a_camScreenPos.first + a_objectPos.first, a_camScreenPos.second + a_objectPos.second };
+    return {a_objectPos.first - a_camScreenPos.first, a_objectPos.second - a_camScreenPos.second};
 }
 
 void Camera::setPos(float newX, float newY){
