a5068ed6cd16dbadfecd719a5e0f26f42be76915
JordiLlinaresSanchez <149331069+JordiLlinaresSanchez@users.noreply.github.com>
2025-02-24 17:20:46 +0100

    transform

--- Carom/src/ECS/JsonEntityParser.h
+++ Carom/src/ECS/JsonEntityParser.h
@@ -1,15 +1,16 @@
 #pragma once
 #include "Entity.h"
 #include "JSONValue.h"
-#include "GameScene.h"
 #include <string>
+
+class ecs::GameScene;
 
 class JsonEntityParser{
 public:
     static ecs::Entity* Parse(ecs::GameScene& gameScene, std::string JSONfile);
 private:
-    void transformComponent(const JSONObject& atributes);
-    void rigidBodyComponent(JSONObject* atributes);
-    void renderTextureComponent(JSONObject* atributes);
+    static void transformComponent(const JSONObject& atributes, ecs::Entity* entity);
+    static void rigidBodyComponent(JSONObject* atributes);
+    static void renderTextureComponent(JSONObject* atributes);
 
 };
