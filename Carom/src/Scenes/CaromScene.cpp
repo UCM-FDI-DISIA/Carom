3d0019603882a3c301dafabee207973160efd751
Mika <mika@ucm.es>
2025-02-24 17:19:27 +0100

    Eso rompia la rama, perdon

--- Carom/src/Scenes/CaromScene.cpp
+++ Carom/src/Scenes/CaromScene.cpp
@@ -28,7 +28,7 @@ CaromScene::CaromScene(State* s, Game* g, GameScene* reward) : GameScene(g), _re
     getEntitiesOfGroup(ecs::grp::WHITEBALL)[0]->getComponent<ecs::RigidBodyComponent>()->applyImpulseToCenter({5.0f, 0.0f});
     createWhiteBall(Vector2D(1, 0), b2_dynamicBody, 2, 1, 1, 1, 10); // ! tst
     getEntitiesOfGroup(ecs::grp::WHITEBALL)[1]->getComponent<ecs::RigidBodyComponent>()->applyImpulseToCenter({-1.0f, 1.0f});
-    addComponent<TryCollisionComponent>(getEntitiesOfGroup(ecs::grp::WHITEBALL)[1]);
+    //addComponent<TryCollisionComponent>(getEntitiesOfGroup(ecs::grp::WHITEBALL)[1]);
     
     _hitManager = new ColorHitManager(this);
     _scoreContainer = new ScoreContainer(200,0);
