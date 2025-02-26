8a0adabb4e3f777cc9f7fa4c19b0cebd818da303
andrea <apariciolopez.andrea@gmail.com>
2025-02-26 16:47:40 +0100

    StartMatchState a medias

--- Carom/src/StateMachine/StartMatchState.h
+++ Carom/src/StateMachine/StartMatchState.h
@@ -2,4 +2,12 @@
 
 #include "State.h"
 
+class StartMatchState: public State {
+public:
+    StartMatchState(CaromScene* scene);
+    bool checkCondition(State*& state) override;
+
+    void onStateEnter() override;
+    void onStateExit() override;
+};
 
