322f7757bbaf308fede2a9ac1af37f94969c674b
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-02-26 01:29:20 +0100

    boceto de clase tweenSystem. no funciona na

---
+++ Carom/src/Tweening/Tweening.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+
+
+class TweenSystem{
+
+    enum TweenType{
+        LINEAR,
+        POW_IN,
+        POW_OUT,
+        POW_IN_OUT
+    };
+
+    struct Tween{
+        float* initialValue;
+        float finalValue;
+        float time;
+        TweenType type;
+        int loop;
+    };
+
+    std::vector<Tween> currentTweens;
+    void addTween(float* initialValue, float endValue,float time, TweenType type, int loop);
+public:
+    void removeTween(float* value);
+    void linearTween(float* initialValue, float endValue, float time, int loop);
+    void powerInTween(float* initialValue, float endValue, float time, int loop);
+    void powerOutTween(float* initialValue, float endValue, float time, int loop);
+    void powerInOutTween(float* initialValue, float endValue, float time, int loop);
+};
