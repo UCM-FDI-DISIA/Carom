944a0ba4f8b0c3711f74f6c30be1a83c7955f2d9
Marco <marcgo19@ucm.es>
2025-02-27 14:09:11 +0100

    Implementado botón torus (falta documentacion)

--- Carom/src/ECS/Button.cpp
+++ Carom/src/ECS/Button.cpp
@@ -3,13 +3,21 @@
 #include "InputHandler.h"
 #include "Texture.h"
 
+#include "PhysicsUtils.h"
+
 namespace ecs
 {
-    Button::Button(Entity* ent) : HandleEventComponent(ent), _onHover(), _onClick(), _onExit(), _isInside() {}
+    Button::Button(Entity* ent) : HandleEventComponent(ent), _onHover(), _onClick(), _onExit(), _isInside() 
+    {
+        _buttonArea = new TextureButton();
+    }
+
+    Button::Button(Entity* ent, ButtonData* buttonType) : HandleEventComponent(ent), _buttonArea(_buttonArea), _onHover(), _onClick(), _onExit(), _isInside()
+    {}
     
     void Button::init()
     {
-        _targetRenderer = _myEntity->getComponent<RenderTextureComponent>();
+        _buttonArea->setTextureComponent(_myEntity->getComponent<RenderTextureComponent>());
         _transform = _myEntity->getComponent<TransformComponent>();
 
         setOnHover([this]() -> void {std::cout << "hover" << std::endl;});
@@ -20,9 +28,8 @@ namespace ecs
     void Button::handleEvent()
     {
         InputHandler* input = InputHandler::Instance();
-        SDL_Rect rect = _targetRenderer->getRect();
 
-        if(input->isMouseInRect(input->getMousePos(), rect)){
+        if(_buttonArea->isMouseInButton(input->getMousePos())){
             if(!_isInside){
                 _isInside = true;
                 _onHover();
@@ -35,5 +42,42 @@ namespace ecs
             _isInside = false;
             _onExit();
         }
+    }
+
+    void Button::ButtonData::setTextureComponent(RenderTextureComponent* targetRenderer)
+    {
+        _targetRenderer = targetRenderer;
+    }
+
+    void Button::ExternalTorusButton::setTextureComponent(RenderTextureComponent* targetRenderer)
+    {
+        ButtonData::setTextureComponent(targetRenderer);
+
+        _internalRadius = std::max(_targetRenderer->getRect().w, _targetRenderer->getRect().h);
+
+        _externalRadius = _internalRadius + (_internalRadius*_externalRadiusFactor);
+    }
+
+    bool Button::ExternalTorusButton::isMouseInButton(std::pair<Sint32, Sint32> mousePos)
+    {
+        SDL_Rect a_textRect = _targetRenderer->getRect();
+        std::pair<Sint32, Sint32> a_buttonCenter = {a_textRect.x+ a_textRect.w/2, a_textRect.y+ a_textRect.h/2};
+        Vector2D a_mouseToButtonVec = 
+        {
+            (float)mousePos.first - (float)a_buttonCenter.first, 
+            (float)mousePos.second - (float)a_buttonCenter.second
+        };
+        float a_vecDistance = a_mouseToButtonVec.magnitude();
+
+        if (a_vecDistance > _internalRadius && a_vecDistance < _internalRadius)
+            return true;
+        else
+            return false;
+    }
+
+    bool Button::TextureButton::isMouseInButton(std::pair<Sint32, Sint32> mousePos)
+    {
+        SDL_Rect rect = _targetRenderer->getRect();
+        return InputHandler::Instance()->isMouseInRect(mousePos, rect);
     }
 }
--- Carom/src/ECS/Button.h
+++ Carom/src/ECS/Button.h
@@ -3,12 +3,45 @@
 #include "RenderTextureComponent.h"
 #include "TransformComponent.h"
 #include <functional>
+#include "SDL.h"
 
 namespace ecs {
     class Button : public ecs::HandleEventComponent
     {
+    public:
+
+        struct ButtonData
+        {
+            virtual bool isMouseInButton(std::pair<Sint32, Sint32> mousePos) = 0;
+
+            /// @brief Pensado para ser llamado dentro del init de Button
+            /// @param _targetRenderer Componente de textura que recibe el click
+            virtual void setTextureComponent(RenderTextureComponent* targetRenderer);
+            
+        protected:
+            RenderTextureComponent* _targetRenderer = nullptr;
+        };
+
+        /// @brief Define un area toroidal a partir de la dimension mayor del objeto renderizable
+        struct ExternalTorusButton: public ButtonData
+        {
+            double _externalRadiusFactor = 1.0;
+
+            void setTextureComponent(RenderTextureComponent* targetRenderer) override;
+            bool isMouseInButton(std::pair<Sint32, Sint32> mousePos) override;
+        private:
+            float _internalRadius;
+            float _externalRadius;
+        };
+
+        struct TextureButton: public ButtonData
+        {
+            bool isMouseInButton(std::pair<Sint32, Sint32> mousePos) override;
+        };
+
+
     private:
-        RenderTextureComponent* _targetRenderer;
+        ButtonData* _buttonArea;
         TransformComponent* _transform;
         std::function<void()> _onHover;
         std::function<void()> _onClick;
@@ -18,6 +51,7 @@ namespace ecs {
 
     public:
         Button(Entity* ent);
+        Button(Entity* ent, ButtonData* buttonType);
         virtual ~Button(){}
 
         void handleEvent() override;
