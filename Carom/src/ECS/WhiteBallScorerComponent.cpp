679e45a64ddea2820a15d204a7ba8bed7d41fbe0
Diego <dicaball@ucm.es>
2025-02-27 12:11:01 +0100

    ScoreContainer eliminado y su funcionalidad trasladada a CaromScene

--- Carom/src/ECS/WhiteBallScorerComponent.cpp
+++ Carom/src/ECS/WhiteBallScorerComponent.cpp
@@ -2,7 +2,6 @@
 #include "RigidBodyComponent.h"
 #include "Entity.h"
 #include "ColorBallScorerComponent.h"
-#include "ScoreContainer.h"
 #include "CaromScene.h"
 #include "WallComponent.h"
 #include <algorithm>
@@ -10,24 +9,22 @@
 namespace ecs{
     void WhiteBallScorerComponent::onCollisionEnter(entity_t other){
 
-            //if is cushion
-            if(_myEntity->tryGetComponent<WallComponent>()) cushions++;
-            else if(other->tryGetComponent<ColorBallScorerComponent>()){
-                CaromScene* a_scene = dynamic_cast<CaromScene*>(&_myEntity->getScene());
-                if(a_scene != nullptr){
-                    if(!previouslyHit){
-                        //scorer.add(1)
-                        a_scene->getScoreContainer()->addScore(1);
-                        previouslyHit = true;
-                    }
-                    else{
-                        //scorer.add(4*2^cushions);
-                        a_scene->getScoreContainer()->addScore(4*pow(2, cushions));
-                        cushions = 0;
-                        previouslyHit = false;
-                    }
+        if(other->tryGetComponent<WallComponent>()) cushions++;
+        else if(other->tryGetComponent<ColorBallScorerComponent>()){
+            CaromScene* a_scene = dynamic_cast<CaromScene*>(&_myEntity->getScene());
+            if(a_scene != nullptr){
+                if(!previouslyHit){
+                    //scorer.add(1)
+                    a_scene->addScore(1);
+                    previouslyHit = true;
                 }
-                
+                else{
+                    //scorer.add(4*2^cushions);
+                    a_scene->addScore(4*pow(2, cushions));
+                    cushions = 0;
+                    previouslyHit = false;
+                }
             }
+        }
     }
 }
--- Carom/src/Scenes/CaromScene.cpp
+++ Carom/src/Scenes/CaromScene.cpp
@@ -3,7 +3,6 @@
 #include "RenderTextureComponent.h"
 #include "RigidBodyComponent.h"
 #include "ColorHitManager.h"
-#include "ScoreContainer.h"
 #include "WhiteBallScorerComponent.h"
 
 #include "PhysicsUtils.h"
@@ -48,7 +47,6 @@ namespace ecs {
         createTable();
 
         _hitManager = new ColorHitManager(this);
-        _scoreContainer = new ScoreContainer(200,0);
     }
 
     entity_t // TODO: provisory definition, add components
@@ -91,8 +89,6 @@ namespace ecs {
         _currentState->onStateEnter();
     }
 
-    State* CaromScene::getCurrentState(){ return _currentState;}
-
     CaromScene::~CaromScene(){
         if(_currentState != nullptr) delete _currentState;
 
@@ -100,7 +96,6 @@ namespace ecs {
         b2DestroyWorld(_myB2WorldId);
 
         delete _hitManager;
-        delete _scoreContainer;
     }
 
     void CaromScene::update(){
@@ -210,7 +205,6 @@ namespace ecs {
 
             sensor->getComponent<ecs::RigidBodyComponent>()->onTriggerEnter(visitor);
         }
-
     }
 
     void
@@ -229,7 +223,19 @@ namespace ecs {
 
     }
 
-    ScoreContainer* CaromScene::getScoreContainer() {return _scoreContainer;}
+    //!Estos métodos asumen la existencia de una clase ScoreUI
+    void CaromScene::addScore(double score) {
+        _currentScore += score;
+        //_scoreUI->refresh(_currentScore, _scoreToBeat);
+    }
 
-    ColorHitManager* CaromScene::getColorHitManager() {return _hitManager; }
+    void CaromScene::removeScore(double score) {
+        _currentScore -= score;
+        //_scoreUI->refresh(_currentScore, _scoreToBeat);
+    }
+
+    void CaromScene::setScoreToBeat(double newScoreToBeat){
+        _scoreToBeat = newScoreToBeat;
+        //_scoreUI->refresh(_currentScore, _scoreToBeat);
+    }
 }
