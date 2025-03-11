6e395290520a73c10871f97c0bae010a298bb504
Javipopotter <javivals@ucm.es>
2025-03-11 16:11:27 +0100

    Update BallEffect.cpp

--- Carom/src/Ball/BallEffect.cpp
+++ Carom/src/Ball/BallEffect.cpp
@@ -6,7 +6,7 @@
 void BallEffect::init()
 {
     _scene = dynamic_cast<ecs::CaromScene*>(&_myEntity->getScene());
-    assert(_scene != nullptr || "Se ha intentado crear");
+    assert(_scene != nullptr || "Se ha intentado agregar un componente de efecto en una escena que no es la de juego");
     _myEntity->getComponent<BallHandler>()->addEffect(this);
 }
 
