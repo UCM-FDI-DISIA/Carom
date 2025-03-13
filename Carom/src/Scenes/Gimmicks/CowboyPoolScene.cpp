4ca7f3ecc3eea7b3f5f3a4926dd702bf622f8eed
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-03-13 23:03:54 +0100

    limpiado initGimmcik

--- Carom/src/Scenes/Gimmicks/CowboyPoolScene.cpp
+++ Carom/src/Scenes/Gimmicks/CowboyPoolScene.cpp
@@ -8,12 +8,6 @@ namespace ecs{
     void CowboyPoolScene::initGimmick(){
         //comportamiento (anyadir entidades de arena en la mesa)
         std::cout<< "CowboyPool Gimmick Instantiated" << std::endl;
-
-        Entity* ent = new Entity(*this, grp::DEFAULT);
-        addComponent<TransformComponent>(ent, b2Vec2{0,0});
-        addComponent<RenderTextureComponent>(ent, &sdlutils().images().at("bola_blanca"), 1000, 1);
-
-        auto tr = getComponent<TransformComponent>(ent);
     }
 
     CowboyPoolScene::CowboyPoolScene(State* state, Game* g, GameScene* reward): CaromScene(state, g, reward){
