679354e006603405c2f7b45d7900a7599a2f64b5
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-03-14 00:40:33 +0100

    hay twink component

--- Carom/src/Scenes/Gimmicks/CowboyPoolScene.cpp
+++ Carom/src/Scenes/Gimmicks/CowboyPoolScene.cpp
@@ -9,16 +9,6 @@ namespace ecs{
     void CowboyPoolScene::initGimmick(){
         //comportamiento (anyadir entidades de arena en la mesa)
         std::cout<< "CowboyPool Gimmick Instantiated" << std::endl;
-
-        Entity* ent = new Entity(*this, grp::DEFAULT);
-        addComponent<TransformComponent>(ent, b2Vec2{0,0});
-        addComponent<RenderTextureComponent>(ent, &sdlutils().images().at("bola_blanca"), 1000, 1);
-        addComponent<TweenComponent>(ent);
-
-        auto a = ent->getComponent<TweenComponent>();
-        a->easePosition({1,1}, 3, tween::EASE_IN_BACK, [=](){
-            a->easePosition({0,0}, 3, tween::EASE_IN_BACK);
-        });
     }
 
     CowboyPoolScene::CowboyPoolScene(State* state, Game* g, GameScene* reward): CaromScene(state, g, reward){
