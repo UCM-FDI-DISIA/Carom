e828337ee4f4455b8b629e54d7759ec4aa612604
Hugodbr <hugodbr@gmail.com>
2025-03-18 14:27:31 +0100

    text display fix render

--- Carom/src/ECS/TextDisplayComponent.cpp
+++ Carom/src/ECS/TextDisplayComponent.cpp
@@ -7,8 +7,8 @@
 #include "SDLUtils.h"
 
 namespace ecs {
-    TextDisplayComponent::TextDisplayComponent(Entity* entity, int renderOrder, float displayScale, std::string initialText, SDL_Color color, std::string key)
-        : RenderTextureComponent(entity, _texture, renderOrder, displayScale), _text(initialText), _color(color), _key(key)
+    TextDisplayComponent::TextDisplayComponent(Entity* entity, int renderLayer, float displayScale, std::string initialText, SDL_Color color, std::string key)
+        : RenderTextureComponent(entity, _texture, renderLayer, displayScale), _text(initialText), _color(color), _key(key)
     {
         reGenerateTexture();
     }
