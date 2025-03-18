b0bee5470555af3c4c915285e35868ec879b4e2c
Diego <dicaball@ucm.es>
2025-03-18 18:44:52 +0100

    Me falta añadirle componentes a la explosion (ayuda?)

--- Carom/src/ECS/GranadeLauncherStickEffect.h
+++ Carom/src/ECS/GranadeLauncherStickEffect.h
@@ -4,6 +4,14 @@
 
 namespace ecs {
     class GranadeLauncherStickEffect : public StickEffectComponent {
+        float _explosionForce, _explosionDelay, _radius;
+    public:
+        __CMPID_DECL__(cmp::GRANADE_LAUCHER_STICK);
 
+        GranadeLauncherStickEffect(entity_t ent, float radius, float explosionForce, float explosionDelay);
+        ~GranadeLauncherStickEffect();
+
+        void init() override;
+        void applyEffect(entity_t ball) override;
     };
 }
