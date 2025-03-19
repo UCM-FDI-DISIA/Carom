eb7ce680c65be4902c1ed63f7bcbc5ece6aa31dd
Javipopotter <javivals@ucm.es>
2025-03-19 16:05:14 +0100

    Update PokeballEffect.cpp

--- Carom/src/Ball/PokeballEffect.cpp
+++ Carom/src/Ball/PokeballEffect.cpp
@@ -10,6 +10,7 @@ void PokeballEffect::onHit(ecs::entity_t ent)
 
     for(Component* comp : effects)
     {
+        std::cout << "Pokeball absorbe efecto: " << typeid(comp).name() << std:: endl;
         // ent->addComponent(comp);
         // ent->removeComponent(&comp);
     }
