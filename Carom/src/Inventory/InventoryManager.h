6ea77136f5599800f4414a21bcbe58b4b0efffbc
Andrea <apariciolopez.andrea@gmail.com>
2025-03-31 16:05:02 +0200

    InventoyManager

---
+++ Carom/src/Inventory/InventoryManager.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+#include "Singleton.h"
+#include "Inventory.h"
+
+class entity_t;
+
+class InventoryManager : public Singleton<InventoryManager> {
+
+	friend Singleton<InventoryManager> ;
+
+public:
+    vector<entity_t> getBalls();
+    entity_t getStick();
+    Perma getPerma();
+
+};
