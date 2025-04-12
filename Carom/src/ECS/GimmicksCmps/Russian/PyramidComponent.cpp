47aac77dce661c7f8dc16cd24f479f7d805de6d4
Hugodbr <hugodbr@gmail.com>
2025-04-12 14:44:27 +0200

    update piramides

--- Carom/src/ECS/GimmicksCmps/Russian/PyramidComponent.cpp
+++ Carom/src/ECS/GimmicksCmps/Russian/PyramidComponent.cpp
@@ -1,4 +1,5 @@
 #include "PyramidComponent.h"
+// #include <iostream>
 
 
 PyramidComponent::PyramidComponent(entity_t ent, const b2Vec2& direction, float slope)
@@ -6,6 +7,9 @@ PyramidComponent::PyramidComponent(entity_t ent, const b2Vec2& direction, float 
     , _direction(direction)
     , _slope(slope)
 {
+    // Tunning:
+    _minForce = 0.018f; // float a_mag = rb->getMass() * _g * sin(M_PI/3.0);
+    _maxForce = 0.10f;
 }
 
 
@@ -19,6 +23,11 @@ PyramidComponent::applyForce(RigidBodyComponent* rb)
 void
 PyramidComponent::calculateMyForceVector(RigidBodyComponent* rb, const Vector2D& direction)
 {
-    float a_mag = rb->getMass() * _g * cos(_slope);
+    float coef      = 3;
+    float vel       = rb->getVelocityMag();
+    float velFactor = _minForce * pow(coef, vel);
+    float a_mag     = b2ClampFloat(velFactor, _minForce, _maxForce);
+
+    // std::cout << "FORCE MAG: " << a_mag << std::endl;
     _myForce = {direction.normalize().getX() * a_mag, direction.normalize().getY() * a_mag};
 }
