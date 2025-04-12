00d07b9a9c15d938471be1a09a9401544c7bc647
acostacion <carmengb27@ucm.es>
2025-04-12 18:38:51 +0200

    los textos fallan pero va

--- Carom/src/Scenes/MenuScenes/MainMenuScene.cpp
+++ Carom/src/Scenes/MenuScenes/MainMenuScene.cpp
@@ -1,43 +1,62 @@
 #include "MainMenuScene.h"
 #include "PoolScene.h"
+#include "ScenesManager.h"
 
 MainMenuScene::MainMenuScene(Game *g) : UIScene(g)
 {
-    createBackground("suelo");
+    entity_t a_bg = createBackground("menuBackground");
 
-    createTable();
+    // --- PANELES.
+    std::vector<entity_t> button;
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectangleMENU", "RectangleMENU")); // no se acciona [0]
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectangleTitle", "RectangleTitle")); // no se acciona [1]
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectanglePlay", "RectanglePlay")); // [2]
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectangleSettings", "RectangleSettings")); // [3]
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectangleControls", "RectangleControls")); // [4]
+    button.emplace_back(createSVGSceneButton("mainMenu", "RectangleCredits", "RectangleCredits")); // [5]
 
-    createText("Carom Titulo de Ejemplo", // text
-        sdlutils().width()/2, // x
-        sdlutils().height()/2 - 250, // y
+    // ajustando fondo.
+    b2Vec2 pos = {a_bg->getTransform()->getPosition().x, a_bg->getTransform()->getPosition().y - PhysicsConverter::pixel2meter(147)};
+    a_bg->getTransform()->setPosition(pos);
+    
+    // --- TEXTOS.
+    createText("Carom", // text
+        button[1]->getTransform()->getPosition().x, // x
+        button[1]->getTransform()->getPosition().y, // y
         2 // size.
     );
 
-    // !!! SE CREA POOLSCENE
-    GameScene *ms = new PoolScene(game); // ! tst 
-
-    entity_t b = createSceneButton(
-        sdlutils().width()/2, // x
-        (sdlutils().height()/2), // y
-        ms // scene
+    createText("Play", // text
+        button[2]->getTransform()->getPosition().x, // x
+        button[2]->getTransform()->getPosition().y, // y
+        1 // scale
     );
+    
+    button[2]->getComponent<Button>()->setOnClick([this](){
+        // !!! SE CREA POOLSCENE
+        GameScene *ms = new PoolScene(game); // ! tst 
+        getGame()->getScenesManager()->pushScene(ms);
+    });  
 
-    createText("Comenzar Partida.", // text
-        sdlutils().width()/2 , // x
-        (sdlutils().height()/2 - 100), // y
+    createText("Settings", // text
+        button[3]->getTransform()->getPosition().x, // x
+        button[3]->getTransform()->getPosition().y, // y
         1 // scale
     );
 
-    // modificar luego, ahora te comienza este tambien partida.
-    entity_t c = createSceneButton(
-        sdlutils().width()/2, // x
-        (sdlutils().height()/2) + 250, // y
-        ms // scene
-    );
-
-    createText("Cargar Partida.", // text
-        sdlutils().width()/2 , // x
-        (sdlutils().height()/2 + 150), // y
+    createText("Controls", // text
+        button[4]->getTransform()->getPosition().x, // x
+        button[4]->getTransform()->getPosition().y, // y
         1 // scale
     );
+
+    createText("Credits", // text
+        button[5]->getTransform()->getPosition().x, // x
+        button[5]->getTransform()->getPosition().y, // y
+        1 // scale
+    );
+
+    for(int i = 1; i < 6; i++){
+        std::cout << "b" << i << ". x: " << button[i]->getTransform()->getPosition().x << "y: " << button[i]->getTransform()->getPosition().y << std::endl;
+    }
 }
