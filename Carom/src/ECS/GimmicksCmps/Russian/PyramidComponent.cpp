841e72e39e1aecfba5b76f6441c25757c008a0a0
Hugodbr <hugodbr@gmail.com>
2025-04-17 11:23:23 +0200

    tune pyramid cmp

--- Carom/src/ECS/GimmicksCmps/Russian/PyramidComponent.cpp
+++ Carom/src/ECS/GimmicksCmps/Russian/PyramidComponent.cpp
@@ -1,5 +1,5 @@
 #include "PyramidComponent.h"
-// #include <iostream>
+#include <iostream>
 
 
 PyramidComponent::PyramidComponent(entity_t ent, const b2Vec2& direction, float slope)
@@ -9,7 +9,9 @@ PyramidComponent::PyramidComponent(entity_t ent, const b2Vec2& direction, float 
 {
     // Tunning:
     _minForce = 0.018f; // float a_mag = rb->getMass() * _g * sin(M_PI/3.0);
-    _maxForce = 0.09f;
+    _maxForce = 0.200f;
+
+    _maxVelocity = 3.0f;
 }
 
 
@@ -26,8 +28,17 @@ PyramidComponent::calculateMyForceVector(RigidBodyComponent* rb, const Vector2D&
     float coef      = 3;
     float vel       = rb->getVelocityMag();
     float velFactor = _minForce * pow(coef, vel);
-    float a_mag     = b2ClampFloat(velFactor, _minForce, _maxForce);
+    float a_mag;
 
-    // std::cout << "FORCE MAG: " << a_mag << std::endl;
-    _myForce = {direction.normalize().getX() * a_mag, direction.normalize().getY() * a_mag};
+    b2Vec2 bodyVel = rb->getVelocity();
+    b2Vec2 forceDirection = {direction.normalize().getX(), direction.normalize().getY()};
+
+    float dotProduct = bodyVel.x * forceDirection.x + bodyVel.y * forceDirection.y;
+
+    if (dotProduct < 0 && vel > _maxVelocity)
+        a_mag = b2ClampFloat(velFactor, _minForce, _maxForce);
+    else
+        a_mag = rb->getMass() * _g * cos(M_PI/3.0);
+
+    _myForce = {forceDirection.x * a_mag, forceDirection.y * a_mag};
 }
