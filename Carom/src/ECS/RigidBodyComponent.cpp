cfb36fad65b6a68b55e76adb7cc53dd767813c88
Marco <marcgo19@ucm.es>
2025-05-05 01:14:55 +0200

    Esto se calcula mal (debería dar grados), está comentado

--- Carom/src/ECS/RigidBodyComponent.cpp
+++ Carom/src/ECS/RigidBodyComponent.cpp
@@ -84,7 +84,7 @@ RigidBodyComponent::getScale() const {
 double
 RigidBodyComponent::getRotation() const {
     b2Rot a_b2r = b2Body_GetRotation(_myB2BodyId);
-    return b2Atan2(a_b2r.s, a_b2r.c);
+    return b2Atan2(a_b2r.s, a_b2r.c);// * 180. / B2_PI;
 }
 
 /// @brief Returns a bool indicating whether the body is moving
