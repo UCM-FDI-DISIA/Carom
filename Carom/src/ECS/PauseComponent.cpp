a0ee23174f5e29f639011eb949f3d7945fa6b1f7
andrea-aparicio-lopez <149385650+guillesc05@users.noreply.github.com>
2025-05-07 17:53:43 +0200

    sonidosç

--- Carom/src/ECS/PauseComponent.cpp
+++ Carom/src/ECS/PauseComponent.cpp
@@ -7,8 +7,11 @@
 #include "ScenesManager.h"
 #include "PauseScene.h"
 
+#include "AudioManager.h"
+
 void PauseComponent::handleEvent(){
     if(InputHandler::Instance()->isKeyDown(SDLK_p)){
+        AudioManager::Instance()->playSoundEfect("drawer_open");
         auto game = _myEntity->getScene().getGame();
         game->getScenesManager()->pushScene(new PauseScene(game, &_myEntity->getScene()));
     }
