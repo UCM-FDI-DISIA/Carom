56c4068d4a62038525214ffdee61679b0fa93d26
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2025-05-07 17:57:05 +0200

    power y charisma

--- Carom/src/Rewards/PermanentRewards/PowerReward.h
+++ Carom/src/Rewards/PermanentRewards/PowerReward.h
@@ -4,18 +4,14 @@
 
 /// @brief Poder: Añade una tirada máxima
 class PowerReward : public Reward {
-protected:
-    int _value;
-
 public:
     PowerReward() : Reward("power", Type::PERMANENT) {
-        auto rng = RNG_Manager::Instance();
-        rng->randomRange(10,51);
+
     }
 
     void applyReward() override {
         auto inv = InventoryManager::Instance();
 
-        inv->setPower(inv->getPower() + _value);
+        inv->setPower(inv->getPower() + 1);
     }
 };
