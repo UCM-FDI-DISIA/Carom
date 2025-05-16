eb40ab7b06658bb57724f19644fb64e1f44cd7b5
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2025-05-16 23:41:32 +0200

    Cosas de caldero

--- Carom/src/Scenes/Rewards/CauldronRewardScene.cpp
+++ Carom/src/Scenes/Rewards/CauldronRewardScene.cpp
@@ -1,7 +1,9 @@
 #include "CauldronRewardScene.h"
+#include "BallInfoDisplayComponent.h"
+
 
 CauldronRewardScene::CauldronRewardScene(Game* game, Reward reward)
-    : RewardScene(game, reward)
+    : RewardScene(game, reward), _randomEffect(NONE)
 {
     #ifdef _DEBUG
         std::cout << "CAULDRON" << std::endl;
@@ -20,10 +22,80 @@ void CauldronRewardScene::applyReward()
 
 void CauldronRewardScene::initObjects()
 {
+    RewardScene::initObjects();
 
+    // Text with info to load
+
+    createSVGImage("win", "Cauldron_Info", "inventory_description_box");
+
+    _randomEffect = Effects (rand() % 8);
+
+    Text title, desc;
+
+    switch(_randomEffect){
+        case BOWLING:
+            title = sdlutils().texts().at("bowling_ballName_pool");
+            desc = sdlutils().texts().at("bowling_ballDesc_pool");
+            break;
+        case X2:
+            title = sdlutils().texts().at("x2_ballName_pool");
+            desc = sdlutils().texts().at("x2_ballDesc_pool");
+            break;
+        case ABBACUS:
+            title = sdlutils().texts().at("abbacus_ballName_pool");
+            desc = sdlutils().texts().at("abbacus_ballDesc_pool");
+            break;
+        case CRISTAL:
+            title = sdlutils().texts().at("cristal_ballName_pool");
+            desc = sdlutils().texts().at("cristal_ballDesc_pool");
+            break;
+        case PETANQUE:
+            title = sdlutils().texts().at("petanque_ballName_pool");
+            desc = sdlutils().texts().at("petanque_ballDesc_pool");
+            break;
+        case POKEBALL:
+            title = sdlutils().texts().at("poke_ballName_pool");
+            desc = sdlutils().texts().at("poke_ballDesc_pool");
+            break;
+        case QUANTIC:
+            title = sdlutils().texts().at("quantic_ballName_pool");
+            desc = sdlutils().texts().at("quantic_ballDesc_pool");
+            break;
+        default:
+            title = sdlutils().texts().at("normal_ballName_pool");
+            desc = sdlutils().texts().at("normal_ballDesc_pool");
+            break;
+    };
+
+    title.text = "Efecto nuevo";
+
+    auto texture = &sdlutils().images().at("inventory_description_box");
+    float scale = static_cast<float>(*&sdlutils().svgs().at("win").at("Cauldron_Info").width) / texture->width();
+    auto svgElem = *&sdlutils().svgs().at("win").at("Cauldron_Info");
+    auto pos = PhysicsConverter::pixel2meter(svgElem.x, svgElem.y);
+    pos.y -= 0.5;
+
+    auto description = new Entity(*this, grp::BALL_INFO_TEXT);
+        addComponent<TransformComponent>(description, pos);
+        addComponent<BallInfoDisplayComponent>(description, 101, 
+                body_t{title.text, title.font, title.color, scale*1.5f},
+                body_t{desc.text, desc.font, desc.color, scale*1.5f}
+                , texture->width() * scale - 60
+                , -texture->width()/2 * scale, -texture->height()/2 * scale
+            );
 }
 
 void CauldronRewardScene::initFunctionalities() 
 {
+    auto balls = openInventory();
 
+    for(auto ball : balls){
+        if(ball.slot == 0) continue;
+
+        ball.button->setOnClick([ball, this](){
+            _selectedBallId = ball.slot;
+            _exitButton->activate();
+            _exitBttText->activate();
+        });
+    }
 }
--- Carom/src/Scenes/Rewards/CauldronRewardScene.h
+++ Carom/src/Scenes/Rewards/CauldronRewardScene.h
@@ -2,8 +2,26 @@
 
 #include "RewardScene.h"
 
+using body_t = BallInfoDisplayComponent::Body;
+
 class CauldronRewardScene : public RewardScene 
-{    
+{
+    friend class Entity;
+
+    enum Effects{
+        ABBACUS,
+        BOWLING,
+        CRISTAL,
+        PETANQUE,
+        POKEBALL,
+        QUANTIC,
+        X2,
+        NONE
+    };
+
+    Effects _randomEffect;
+    int _selectedBallId;
+
 public:
     CauldronRewardScene(Game* game, Reward reward);
     virtual ~CauldronRewardScene();
