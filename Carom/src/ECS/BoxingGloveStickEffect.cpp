62c7230d7be96f2c0ec8b5edcdf4b1d10bd4e577
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-05-17 23:59:38 +0200

    boxing sound

--- Carom/src/ECS/BoxingGloveStickEffect.cpp
+++ Carom/src/ECS/BoxingGloveStickEffect.cpp
@@ -1,6 +1,7 @@
 #include "BoxingGloveStickEffect.h"
 #include "FrictionMultiplierEffect.h"
 #include "Entity.h"
+#include "AudioManager.h"
 
 
 BoxingGloveStickEffect::BoxingGloveStickEffect(entity_t entity, float factor)  : StickEffectComponent(entity), _factor(factor)
@@ -11,6 +12,7 @@ BoxingGloveStickEffect::~BoxingGloveStickEffect() {}
 
 void
 BoxingGloveStickEffect::applyEffect(entity_t target) {
+    AudioManager::Instance()->playSoundEfect("boxing");
     target->addComponent<FrictionMultiplierEffect>(new FrictionMultiplierEffect(target, _factor));
 }
 
