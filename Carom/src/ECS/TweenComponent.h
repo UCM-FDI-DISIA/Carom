a3295921ff0dcc4bbe5491b9145a478ca05e0bd2
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-05-18 16:56:07 +0200

    documentado tweens

--- Carom/src/ECS/TweenComponent.h
+++ Carom/src/ECS/TweenComponent.h
@@ -32,14 +32,59 @@ public:
     virtual ~TweenComponent();
     void init() override;
     void update() override;
+    //Pausa/despausa los tweens
     inline void setPause(bool b){_paused = b;}
 
+    //Interpolacion linear de un valor por referencia
+    //@param value Valor inicial, y que se va a actualizar constantemente
+    //@param finalValue Valor final que va a tomar value
+    //@param duration Duracion de la interpolacion
+    //@param type Tipo de interpolacion 
+    //@param loop Repeticion en bucle de la misma interpolacion, invirtiendose al alcanzar el final
+    //@param callback Callback que se ejecuta al alcanzar el final
+    //@param onUpdate Callback que se ejecuta en cada iteracion de update()
     void easeValue(float* value, float finalValue, float duration, tween::tweenType type, bool loop = false , Callback callback = [](){}, Callback onUpdate = [](){});
+    
+    //Interpolacion linear de la posicion de la entidad
+    //@param finalPos Valor final que va a tomar la posicion de la entidad
+    //@param duration Duracion de la interpolacion
+    //@param type Tipo de interpolacion
+    //@param loop Repeticion en bucle de la misma interpolacion, invirtiendose al alcanzar el final
+    //@param callback Callback que se ejecuta al alcanzar el final
+    //@param onUpdate Callback que se ejecuta en cada iteracion de update()
     void easePosition(b2Vec2 finalPos, float duration, tween::tweenType type,bool loop = false, Callback callback = [](){}, Callback onUpdate= [](){});
+    
+    //Interpolacion linear de la rotacion de la entidad
+    //@param finalRot Valor final que va a tomar la rotacion de la entidad
+    //@param duration Duracion de la interpolacion
+    //@param type Tipo de interpolacion
+    //@param loop Repeticion en bucle de la misma interpolacion, invirtiendose al alcanzar el final
+    //@param callback Callback que se ejecuta al alcanzar el final
+    //@param onUpdate Callback que se ejecuta en cada iteracion de update()
     void easeRotation(float finalRot, float duration, tween::tweenType type,bool loop = false, Callback callback = [](){}); 
+
+    //Interpolacion linear de la rotacion de la entidad
+    //@param finalRot Valor final que va a tomar la rotacion de la entidad
+    //@param pivotPoint Punto de pivote para la rotacion
+    //@param duration Duracion de la interpolacion
+    //@param type Tipo de interpolacion
+    //@param loop Repeticion en bucle de la misma interpolacion, invirtiendose al alcanzar el final
+    //@param callback Callback que se ejecuta al alcanzar el final
+    //@param onUpdate Callback que se ejecuta en cada iteracion de update()
     void easeRotation(float finalRot, b2Vec2 pivotPoint, float duration, tween::tweenType type,bool loop = false, Callback callback = [](){}); 
+
+    //Interpolacion linear de la escala de la entidad
+    //@param finalScale Valor final que va a tomar la escala de la entidad
+    //@param duration Duracion de la interpolacion
+    //@param type Tipo de interpolacion
+    //@param loop Repeticion en bucle de la misma interpolacion, invirtiendose al alcanzar el final
+    //@param callback Callback que se ejecuta al alcanzar el final
+    //@param onUpdate Callback que se ejecuta en cada iteracion de update()
     void easeScale(float finalScale, float duration, tween::tweenType type,bool loop = false, Callback callback = [](){});
 
+    //Borra todos los tweens en curso
     void eraseAllTweens();
+
+    //Devuelve true si hay alguna interpolacion en curso
     inline bool isTweening() {return _tweens.size() >0;}
 };
