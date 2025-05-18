c9302917ed9e990c9bb046d60e0541852edd7e2d
guillesc05 <149385650+guillesc05@users.noreply.github.com>
2025-05-18 17:26:54 +0200

    ultimas cosas

--- Carom/src/ECS/WobblyRenderTextureComponent.h
+++ Carom/src/ECS/WobblyRenderTextureComponent.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "TextDisplayComponent.h"
 
+
+//Hereda de TextDisplayComponent, y lo único que hace es sobreescribir el método render para poder renderizar el texto con vértices de SDL. Con esto, se puede simular un shader de movimiento ya que en cada iteración los vértices se van moviendo dando ese efecto de ola.
 class WobblyRenderTextureComponent: public TextDisplayComponent{
     const int VERTICES_BY_WIDTH = 12;
     float _speed = 7;
