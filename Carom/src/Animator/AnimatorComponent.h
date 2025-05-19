14f2d0c8c275c5ec9ff759c1b35d7680da329915
Marco <marcgo19@ucm.es>
2025-05-19 13:05:15 +0200

    partes del animator comentada

--- Carom/src/Animator/AnimatorComponent.h
+++ Carom/src/Animator/AnimatorComponent.h
@@ -10,26 +10,37 @@ class ITransform;
 struct Animation;
 struct SDL_Rect;
 
+/// @brief Componente encargado de cambiar el frame de renderspritesheetcomponent (es dependiente de este otro)
 class AnimatorComponent : public LogicComponent {
 private:
     RenderSpritesheetComponent* _myRenderer;
     Animation* _animation;
+
+    /// @brief El indice que del frame que está activo en la animación en este momento
     int _frameIndex;
+
+    /// @brief timestamp de cuando comenzó el último frame
     Uint32 _frameStartTimestamp;
 
-    void animationEnd(Animation::AnimationType);
+    /// @brief Es llamado cuando se agota el tiempo del último frame de la aniamación
+    /// @param animType Se debe pasar el animType de _animation
+    void animationEnd(Animation::AnimationType animType);
+
+    /// @brief Reinicia la animación al primer frame (índice 0)
+    void restartAnimation();
 public:
     __CMPID_DECL__(cmp::ANIMATOR);
 
+    /// @brief
+    /// @param ent 
+    /// @param anim La animación suele pasarse mediante sdlutils().animations().at("id_de_animacion")
     AnimatorComponent(entity_t ent,  Animation* anim);
     ~AnimatorComponent() {}
 
     void init() override;
     void update() override;
 
-    void restartAnimation();
-    void setAnimation(Animation* newAnim) {
-        _animation = newAnim; 
-        _frameIndex = 0;
-        _myRenderer->setFrame(_animation->_frameList[_frameIndex].frame);}
+    /// @brief Cambia la animación por newAnim
+    /// @param newAnim 
+    void setAnimation(Animation* newAnim);
 };
--- Carom/src/Animator/BallRollerAnimatorComponent.h
+++ Carom/src/Animator/BallRollerAnimatorComponent.h
@@ -4,15 +4,26 @@
 #include "RigidBodyComponent.h"
 #include <vector>
 
+/// @brief Componente similar a AnimatorComponent, solo que en vez de utilizar una animación para modificar los frames
+///        toma en cuenta la velocidad de su rigidbody (se pretende que sea el de una bola con spritesheet)
+///        
+///        Idealmente ambos animatorcomponents tendrían un padre con las cosas en común
 class BallRollerAnimatorComponent : public LogicComponent {
 private:
     RenderSpritesheetComponent* _myRenderer;
+    /// @brief rigidbody del objeto a animar para obtener su velocidad
     RigidBodyComponent* _myRigidbody;
 
+    /// @brief numero total de frames del spritesheet, se obtiene en init con un get sobre _myRenderer
     int _frameAmount;
+    /// @brief indice del frame actual
     int _frameIndex;
+    /// @brief La velocidad que equivale a un cambio de frame
     float _frameChangeThreshold;
 
+    /// @brief una vez se cambia de frame a veces queda velocidad sobrante, se guarda 
+    ///        para el proximo update para que la animación sea lo mas consistente con
+    ///        la velocidad prosible
     float _residualSpeed = .0;
 public:
     __CMPID_DECL__(cmp::ANIMATOR);
@@ -21,5 +32,8 @@ public:
     ~BallRollerAnimatorComponent() {}
 
     void init() override;
+
+    /// @brief Obtiene la velocidad en ese momento del _myRigidbody y actualiza los
+    ///        frames acorde a todos los valores privados que no son componentes
     void update() override;
 };
