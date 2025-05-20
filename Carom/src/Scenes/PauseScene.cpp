0b264b58f0821e079bb4fa86b4c0af5ff0eabc2b
Marco <marcgo19@ucm.es>
2025-05-20 23:00:39 +0200

    build final

--- Carom/src/Scenes/PauseScene.cpp
+++ Carom/src/Scenes/PauseScene.cpp
@@ -23,7 +23,7 @@ PauseScene::PauseScene(Game* g, GameScene* scene): UIScene(g) {
 
 PauseScene::~PauseScene(){
     AudioManager::Instance()->setVolumeMusicTrack(PAUSE_THEME, 0);
-    AudioManager::Instance()->setVolumeMusicTrack(trackName(previousTheme), 64);
+    AudioManager::Instance()->setVolumeMusicTrack(trackName(previousTheme), 40);
 }
 
 void
